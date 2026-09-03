b831cdf3bbbf20bbcbc671d6add04f98cde899a8
udithindrachapa <udithindrachapa26@gmail.com>
2026-09-03 08:36:54 +0530

    Hosting

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import mysql from "mysql2/promise";
+/*import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -9,6 +9,24 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+export default pool;*/
+
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
 
 export default pool;
